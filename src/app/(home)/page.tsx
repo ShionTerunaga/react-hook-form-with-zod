7f7adfa8b79d4f10794e5ff4704b25227da610b0
teru12012000 <sion2769@gmail.com>
2025-02-20 00:13:56 +0900

    feat: first commit

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import { Form } from "./components/form";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>form verify</h1>
+            <Form />
+        </div>
+    );
+}
